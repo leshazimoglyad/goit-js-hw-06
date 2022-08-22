209faf919a542ffa7d5c3d1e976e2ea98995e801
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-08-22 15:51:11 +0300

    dz

--- js/task-09.js
+++ js/task-09.js
@@ -1,9 +1,22 @@
-const buttonRef = document.querySelector(".change-color");
-const spanEl = document.querySelector(".color");
-buttonRef.addEventListener("click", () => {
-  spanEl.textContent = getRandomHexColor();
-  document.body.setAttribute(
-    "style",
-    "background-color: " + spanEl.textContent
-  );
-});
+// const buttonRef = document.querySelector(".change-color");
+// const spanEl = document.querySelector(".color");
+// buttonRef.addEventListener("click", () => {
+//   spanEl.textContent = getRandomHexColor();
+//   document.body.setAttribute(
+//     "style",
+//     "background-color: " + spanEl.textContent
+//   );
+// });
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.body;
+const span = document.querySelector(".color");
+const chgBtn = document.querySelector(".change-color");
+chgBtn.addEventListener("click", changeBgnd);
+function changeBgnd() {
+  const ourColor = getRandomHexColor();
+  body.style.backgroundColor = `${ourColor}`;
+  span.textContent = `${ourColor}`;
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const addButton = document.querySelector("[data-create]");
+const delButton = document.querySelector("[data-destroy]");
+const divElts = document.querySelector("#boxes");
+
+addButton.addEventListener("click", addButtonHandler);
+function addButtonHandler() {
+  createBoxes(input.value);
+}
+
+delButton.addEventListener("click", () => {
+  document.querySelectorAll(".box").forEach((e) => e.remove());
+  input.value = "";
+});
+
+// const divs = [];
+function createBoxes(number) {
+  // упоротый читерский for чтоб не обнулять массив divs и размер фигуры перед следующим созданием
+  for (var divs = [], i = 0, size = 30; i < number; i++, size += 10) {
+    const div = document.createElement("div");
+    div.style.backgroundColor = getRandomHexColor();
+    div.classList.add("box");
+    div.style.height = `${size}px`;
+    div.style.width = `${size}px`;
+    divs.push(div);
+    console.log(div);
+  }
+  divElts.append(...divs);
+  // divs.length = 0;
+}
