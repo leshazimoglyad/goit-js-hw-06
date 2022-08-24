1e389c670d698b1b147f037864f241c71e839084
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-08-24 10:10:12 +0300

    вч

--- js/task-03.js
+++ js/task-03.js
@@ -12,12 +12,24 @@ const images = [
     alt: "Group of Horses Running",
   },
 ];
-const imagesEl = document.querySelector(`#images`);
-const img = [...images];
-for (let el of images)
-  document
-    .querySelector(".gallery")
-    .insertAdjacentHTML(
-      "beforeEnd",
-      `<li><img src="${el.url}" alt="${el.alt}"></li>`
-    );
+// const imagesEl = document.querySelector(`#images`);
+// const img = [...images];
+// for (let el of images)
+//   document
+//     .querySelector(".gallery")
+//     .insertAdjacentHTML(
+//       "beforeEnd",
+//       `<li><img src="${el.url}" alt="${el.alt}"></li>`
+//     );
+document.head.insertAdjacentHTML(
+  "beforeend",
+  // "<style>.flex-container {display: flex; flex-wrap: nowrap; flex-direction: column;}.flex-item {padding: 5px 5px 5px 5px; margin: 20px 20px 20px 20px; list-style-type: none;}</style>"
+  "<style>.flex-container {display: flex; flex-wrap: wrap}.flex-item {padding: 5px 5px 5px 5px; margin: 20px 20px 20px 20px; list-style-type: none;}</style>"
+);
+
+const picGallery = document.querySelector(".gallery");
+picGallery.classList.add("flex-container");
+const pictArr = images.map((image) => {
+  return `<li class="flex-item"><img src="${image.url}" alt="${image.alt}" width="450" height="300"></li>`;
+});
+picGallery.insertAdjacentHTML("afterbegin", pictArr.join(""));
